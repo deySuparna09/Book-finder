ae61dc48b1c28a784cbb182727791c8897ca88ff
deySuparna09 <suparnadey126@gmail.com>
2025-10-31 21:46:43 +0530

    Added background colour and add browser logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,30 +29,28 @@ const Home = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center text-blue-700 mb-8">
-        📚 Book Finder
-      </h1>
+    <div className="min-h-screen bg-[#f7faff] p-6">
 
-      {/* Subtitle */}
-      <p className="text-gray-600 text-center text-sm md:text-lg mb-6 max-w-md">
-        Discover your favorite book by searching below
-      </p>
+  <h1 className="text-3xl font-bold text-center text-blue-700 mb-8">
+    📚 Book Finder
+  </h1>
 
-      <SearchBar onSearch={fetchBooks} />
+  <p className="text-gray-600 text-center text-sm md:text-lg mb-6 max-w-md mx-auto">
+    Discover your favorite book by searching below
+  </p>
 
-      {loading && (
-        <p className="text-center text-gray-600">Loading books...</p>
-      )}
+  <SearchBar onSearch={fetchBooks} />
 
-      {error && <p className="text-center text-red-600">{error}</p>}
+  {loading && <p className="text-center text-gray-600 mt-4">Loading books...</p>}
+  {error && <p className="text-center text-red-600 mt-4">{error}</p>}
 
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
-        {books.map((book) => (
-          <BookCard key={book.key} book={book} />
-        ))}
-      </div>
-    </div>
+  <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+    {books.map((book) => (
+      <BookCard key={book.key} book={book} />
+    ))}
+  </div>
+</div>
+
   );
 };
 
